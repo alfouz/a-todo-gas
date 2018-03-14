0737b8d30ed2e208d4bc39a1f708a800c3988bf5
Jorge Lopez Seijas <jorginho@riseup.net>
2018-03-14 11:45:36 +0100

    add healthResultsActivity y conexiones con los diferentes apartados

--- brainyCar/app/src/main/java/com/atodogas/brainycar/HealthResultsActivity.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/HealthResultsActivity.java
@@ -10,19 +10,43 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class HealthResultsActivity extends AppCompatActivity {
     private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_health_results);
         // Setting action bar
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle("Estado bluetooth");
+
+        ListView erroresListView = findViewById(R.id.erroresListView);
+        String[] errores = { "P0123", "B0523", "P1234", "C0983", "B0983", "P1983"};
+        erroresListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, errores));
+
+        erroresListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                Context context = getApplicationContext();
+                CharSequence text = "clicked on " + selectedItem;
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                Log.d(TAG, text.toString());
+            }
+        });
     }
 
     @Override
@@ -60,6 +84,22 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    /** Called when the user taps the "health" menu button */
+    public void home(View view) {
+        Log.d(TAG, "clicked button home");
+
+        Context context = getApplicationContext();
+        CharSequence text = "clicked button home";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        //TODO esto deberia llevar a la activity del boton grande de diagnostico que actualmente no esta creada
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
     public void drive(View view) {
         Log.d(TAG, "clicked button drive");
 
@@ -94,22 +134,6 @@ public class HomeActivity extends AppCompatActivity {
         toast.show();
     }
 
-    /** Called when the user taps the "health" menu button */
-    public void health(View view) {
-        Log.d(TAG, "clicked button health");
-
-        Context context = getApplicationContext();
-        CharSequence text = "clicked button health";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-        //TODO esto deberia llevar a la activity del boton grande de diagnostico que actualmente no esta creada
-        Intent intent = new Intent(this, HealthResultsActivity.class);
-        startActivity(intent);
-    }
-
     /** Called when the user taps the "location" menu button */
     public void historic(View view) {
         Log.d(TAG, "clicked button historic");
@@ -128,6 +152,17 @@ public class HomeActivity extends AppCompatActivity {
 
         Context context = getApplicationContext();
         CharSequence text = "clicked button profile";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void restartHealth(View view){
+        Log.d(TAG, "clicked button restart health");
+
+        Context context = getApplicationContext();
+        CharSequence text = "clicked button restart health";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);

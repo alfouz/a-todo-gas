713d282c525c834ceca3c0932d54fc7231850b3b
Adrián López Martínez <adrianlm.naron@gmail.com>
2018-04-04 20:10:48 +0200

    Pantallas historial y perfil

--- brainyCar/app/src/main/java/com/atodogas/brainycar/HistoricFragment.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/HistoricFragment.java
@@ -3,9 +3,11 @@ package com.atodogas.brainycar;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class HistoricFragment extends Fragment {
 
+    private View root;
 
     public HistoricFragment() {
         // Required empty public constructor
@@ -22,8 +25,31 @@ public class HistoricFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //change action bar title
+        MainActivity activity = (MainActivity) getActivity();
+        activity.changeActionBarTitle("Histórico");
+
+        root = inflater.inflate(R.layout.fragment_historic, container, false);
+
+        TabHost host = (TabHost) root.findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Viajes");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Viajes");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Estadisticas");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Estadisticas");
+        host.addTab(spec);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historic, container, false);
+        return root;
     }
 
 }
--- brainyCar/app/src/main/java/com/atodogas/brainycar/ProfileFragment.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/ProfileFragment.java
@@ -1,11 +1,13 @@
 package com.atodogas.brainycar;
 
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    private View root;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -22,8 +25,31 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //change action bar title
+        MainActivity activity = (MainActivity) getActivity();
+        activity.changeActionBarTitle("Perfil");
+
+        root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Resources res = getResources();
+
+        TabHost tabs= root.findViewById(R.id.my_host);
+        tabs.setup();
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.Resumen);
+        spec.setIndicator("Resumen",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.Logros);
+        spec.setIndicator("Logros",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return root;
     }
 
 }

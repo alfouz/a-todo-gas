34ab58661d9c58cf97980b98117029c16e20eb73
belenVarela <belen.varela@udc.es>
2018-04-11 17:28:59 +0200

    home bluetooth + design

--- brainyCar/app/src/main/java/com/atodogas/brainycar/HomeFragment.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/HomeFragment.java
@@ -2,17 +2,23 @@ package com.atodogas.brainycar;
 
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.graphics.drawable.LayerDrawable;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Set;
 
 
 /**
@@ -21,6 +27,9 @@ import android.widget.Toast;
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private static final String TAG = HomeFragment.class.getSimpleName();
+    private static final int REQUEST_ENABLE_BT = 1;
+    private MainActivity mainActivity;
+    private String bluetoothState;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,16 +37,107 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
-        //change action bar title
-        MainActivity activity = (MainActivity) getActivity();
-        activity.changeActionBarTitle("Estado bluetooth");
+        mainActivity = (MainActivity) getActivity();
 
         Button driveButton = view.findViewById(R.id.driveButton);
         driveButton.setOnClickListener(this);
         View lastTripLayout = view.findViewById(R.id.lastTripLayout);
         lastTripLayout.setOnClickListener(this);
 
+        //**************************************BLUETOOTH**************************************
+        //we see if the device has bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            bluetoothState = getResources().getString(R.string.bluetoothUnable);
+        } else {
+            // We see if it is enabled and if not we enable it
+            if (!mBluetoothAdapter.isEnabled()) {
+                bluetoothState = getResources().getString(R.string.bluetoothConecting);
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                bluetoothState = getResources().getString(R.string.bluetoothOn);
+            }
+
+            //TODO probando a mostrar dispositivos sincronizados
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            // If there are paired devices
+            if (pairedDevices.size() > 0) {
+                // Loop through paired devices
+                Log.d(TAG, "Dispositivos sincronizados por bluetooth:");
+                for (BluetoothDevice device : pairedDevices) {
+                    // Add the name and address to an array adapter to show in a ListView
+                    Log.d(TAG, device.getName() + "\n" + device.getAddress());
+                }
+            }
+        }
+        mainActivity.changeActionBarTitle(bluetoothState);
+
+
+        //**************************************DATA**************************************
+        // CAR BRAND
+        TextView cardBrandElement = view.findViewById(R.id.carBrand);
+        cardBrandElement.setText("VW Golf TDI 2011");
+
+        //LAST TRIP INFO
+
+        TextView lastTripDateElement = view.findViewById(R.id.lastTripDate);
+        lastTripDateElement.setText("17/01/18");
+
+        TextView firstTripTimeElement = view.findViewById(R.id.firstTripTime);
+        firstTripTimeElement.setText("18:00h");
+
+        TextView firstPlaceElement = view.findViewById(R.id.firstPlace);
+        firstPlaceElement.setText("Madrid");
+
+        TextView secondTripTimeElement = view.findViewById(R.id.secondTripTime);
+        secondTripTimeElement.setText("23:30h");
+
+        TextView secondPlaceElement = view.findViewById(R.id.secondPlace);
+        secondPlaceElement.setText("A Coruña");
+
+        TextView tripDashboardValueElement = view.findViewById(R.id.tripDashboardValue);
+        tripDashboardValueElement.setText("591km");
+
+        TextView tripHourglassValueElement = view.findViewById(R.id.tripHourglassValue);
+        tripHourglassValueElement.setText("5h 30m");
+
+        TextView tripGasolineValueElement = view.findViewById(R.id.tripGasolineValue);
+        tripGasolineValueElement.setText("72l");
+
+        //GENERAL INFO
+
+        TextView dashboardValueElement = view.findViewById(R.id.dashboardValue);
+        dashboardValueElement.setText("2500km");
+
+        TextView gasolineValueElement = view.findViewById(R.id.gasolineValue);
+        gasolineValueElement.setText("25/50l");
+
+        TextView coolantValueElement = view.findViewById(R.id.coolantValue);
+        coolantValueElement.setText("4l");
+
         return view;
+    }
+
+    // Results bluetooth request
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                switch(resultCode) {
+                    case Activity.RESULT_OK:
+                        bluetoothState = getResources().getString(R.string.bluetoothOn);
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        bluetoothState = getResources().getString(R.string.bluetoothOff);
+                }
+                mainActivity.changeActionBarTitle(bluetoothState);
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
     }
 
     @Override
@@ -54,9 +154,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public void drive() {
         Log.d(TAG, "clicked button drive");
-
         DashboardFragment dashboarFragment= new DashboardFragment();
-        getActivity().getSupportFragmentManager().beginTransaction()
+        mainActivity.getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, dashboarFragment)
                 .addToBackStack(null)
                 .commit();
@@ -64,7 +163,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public void tripDetail() {
         Log.d(TAG, "clicked to see details of last trip");
-        Toast.makeText(getActivity(),"clicked to see details of last trip",Toast.LENGTH_SHORT).show();
+        Toast.makeText(mainActivity,"clicked to see details of last trip",Toast.LENGTH_SHORT).show();
     }
 
 }

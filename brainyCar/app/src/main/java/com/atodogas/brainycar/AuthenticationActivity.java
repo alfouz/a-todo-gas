5e167428f01ae48027cec529974287ae80508b74
David Miguel Lago Solla <david.lago@udc.es>
2018-04-24 17:18:15 +0200

    logout button

--- brainyCar/app/src/main/java/com/atodogas/brainycar/AuthenticationActivity.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/AuthenticationActivity.java
@@ -3,6 +3,7 @@ package com.atodogas.brainycar;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;

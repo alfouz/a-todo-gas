934a57ba6160ae842c0a3337779591798b128821
Jorge Lopez Seijas <jorginho@riseup.net>
2018-05-02 13:48:47 +0200

    add AsyncTask ConnectOBD que realiza el proceso de obtención del OBD de entre los dispositivos emparejados

---
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/AsyncTasks/ConnectOBD.java
@@ -0,0 +1,56 @@
+package com.atodogas.brainycar.AsyncTasks;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.AsyncTask;
+
+import com.atodogas.brainycar.OBD.OBDAdapter;
+import com.atodogas.brainycar.OBD.OBDReal;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ConnectOBD extends AsyncTask<Void, Void, OBDAdapter> {
+    CallbackInterface callback;
+
+    public ConnectOBD(CallbackInterface callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected OBDAdapter doInBackground(Void... voids) {
+        OBDAdapter adapter = null;
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+        for (BluetoothDevice device : btAdapter.getBondedDevices()){
+            BluetoothSocket socket = null;
+            try {
+                socket = device.createRfcommSocketToServiceRecord(uuid);
+                socket.connect();
+                adapter = new OBDReal(socket.getInputStream(), socket.getOutputStream());
+
+                if(adapter.isConnected()){
+                    break;
+                }
+
+                adapter = null;
+            } catch (IOException e) {
+                adapter = null;
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                adapter = null;
+                e.printStackTrace();
+            }
+        }
+
+        return adapter;
+    }
+
+    @Override
+    protected void onPostExecute(OBDAdapter obdAdapter) {
+        super.onPostExecute(obdAdapter);
+        callback.doCallback(obdAdapter);
+    }
+}

810ff1533ed7cea07002db360913b560983d92d5
David Miguel Lago Solla <david.lago@udc.es>
2018-05-03 17:51:15 +0200

    fix feched in drive mode in stop button

--- brainyCar/app/src/main/java/com/atodogas/brainycar/DashboardActivity.java
+++ brainyCar/app/src/main/java/com/atodogas/brainycar/DashboardActivity.java
@@ -105,6 +105,8 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         Intent intent = new Intent(this, MainActivity.class);
         int idUser = getIntent().getIntExtra("idUser", - 1);
         intent.putExtra("idUser", idUser);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
